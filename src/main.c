7f94d32184c459d70b2bc3b47f107dc6e1ff88d6
Agustin Ranieri <aguseranieri@gmail.com>
2024-03-16 18:10:11 +0000

    Incluyo las commons

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <commons/string.h>
 
 int main(int argc, char* argv[]) {
-    puts("Hola mundo!!!");
+    char *nombre = "Operativos";
+    if (argc >= 2) {
+        nombre = argv[1];
+    }
+
+    char *saludo = string_from_format("Hola, %s!!", nombre);
+    puts(saludo);
+    free(saludo);
+
     return 0;
 }
