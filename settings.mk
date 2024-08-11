7c53366bdef31f9cbfbcc27b6bf856d882f7f46b
Agustin Ranieri <aguseranieri@gmail.com>
2024-08-11 16:36:58 -0300

    Update build

--- settings.mk
+++ settings.mk
@@ -2,15 +2,12 @@
 LIBS=commons pthread readline m
 
 # Custom libraries' paths
+SHARED_LIBPATHS=
 STATIC_LIBPATHS=
 
 # Compiler flags
 CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
-CRELEASE=-O3 -Wall -DNDEBUG -fcommon
+CRELEASE=-O3 -Wall -DNDEBUG
 
-# Arguments when executing with start, memcheck or helgrind
-ARGS=
-
-# Valgrind flags
-MEMCHECK_FLAGS=--track-origins=yes
-HELGRIND_FLAGS=
+# Source files (*.c) to be excluded from tests compilation
+TEST_EXCLUDE=src/main.c
